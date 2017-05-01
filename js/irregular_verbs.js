69f8e19404b423552fa1e92e844d6e2218639dec
Gustavo Apolinario <gustavoapolinario@MacBook-Pro-de-Gustavo.local>
2017-05-01 15:23:17 -0300

    commit for test app in cloud

---
+++ js/irregular_verbs.js
@@ -0,0 +1,222 @@
+"use strict";
+var irregular_verbs = (function(){
+	var in_infinitive = "";
+	var in_past = "";
+	var in_past_participle = "";
+	var translated = "";
+
+	var points_correct = 0;
+	var points_wrong = 0;
+	var points_consective = 0;
+	var points_max_consective = 0;
+
+
+	var verbs = [
+		['to be', 'was/were', 'been', 'ser/estar'],
+		['to beat', 'beat', 'beat', 'bater'],
+		['to become', 'became', 'become', 'tornar-se'],
+		['to begin', 'began', 'begun', 'começar'],
+		['to bend', 'bent', 'bent', 'dobrar'],
+		['to bet', 'bet', 'bet', 'apostar'],
+		['to bite', 'bit', 'bit', 'morder'],
+		['to blow', 'blew', 'blown', 'soprar'],
+		['to break', 'broke', 'broken', 'quebrar'],
+		['to bring', 'brought', 'brought', 'trazer'],
+		['to build', 'built', 'built', 'construir'],
+		['to buy', 'bought', 'bought', 'comprar'],
+		['to catch', 'caught', 'caught', 'pegar'],
+		['to choose', 'chose', 'chosen', 'escolher'],
+		['to come', 'came', 'come', 'vir'],
+		['to cost', 'cost', 'cost', 'custar'],
+		['to cut', 'cut', 'cut', 'cortar'],
+		['to deal', 'dealt', 'dealt', 'lidar com'],
+		['to dig', 'dug', 'dug', 'cavar'],
+		['to do', 'did', 'done', 'fazer'],
+		['to draw', 'drew', 'drawn', 'desenhar'],
+		['to drink', 'drank', 'drunk', 'beber'],
+		['to drive', 'drove', 'driven', 'dirigir'],
+		['to eat', 'ate', 'eaten', 'comer'],
+		['to fall', 'fell', 'fallen', 'cair'],
+		['to feed', 'fed', 'fed', 'alimentar'],
+		['to feel', 'felt', 'felt', 'sentir'],
+		['to fight', 'fought', 'fought', 'lutar'],
+		['to find', 'found', 'found', 'achar'],
+		['to fit', 'fit', 'fit', 'servir'],
+		['to forget', 'forgot', 'forgotten', 'esquecer'],
+		['to fly', 'flew', 'flown', 'voar'],
+		['to freeze', 'froze', 'frozen', 'congelar'],
+		['to get', 'got', 'gotten', 'pegar'],
+		['to give', 'gave', 'given', 'dar'],
+		['to go', 'went', 'gone', 'ir'],
+		['to grow', 'grew', 'grown', 'crescer'],
+		['to have', 'had', 'had', 'ter'],
+		['to hear', 'heard', 'heard', 'escutar'],
+		['to hide', 'hid', 'hidden', 'esconder'],
+		['to hold', 'held', 'held', 'segurar'],
+		['to hurt', 'hurt', 'hurt', 'machucar'],
+		['to keep', 'kept', 'kept', 'manter'],
+		['to know', 'knew', 'known', 'saber'],
+		['to learn', 'learnt', 'learnt', 'aprender'],
+		['to leave', 'left', 'left', 'sair'],
+		['to lend', 'lent', 'lent', 'emprestar'],
+		['to let', 'let', 'let', 'deixar'],
+		['to lose', 'lost', 'lost', 'perder'],
+		['to make', 'made', 'made', 'fazer'],
+		//['aaaaaaaa', 'aaaaaaaa', 'aaaaaaaa', 'aaaaaaaa'],
+	];
+	var latest_verbs = [];
+
+	var randomNewVerb = function() {
+
+		if( verbs.length == 0 ) {
+			finishedGame();
+			return;
+		}
+
+
+		var rand = Math.floor( Math.random() * ( verbs.length ) );
+
+		[in_infinitive, in_past, in_past_participle, translated] = verbs[ rand ];
+
+		document.querySelector('#infinitive').innerHTML = in_infinitive;
+		document.querySelector('#past').value = '';
+		document.querySelector('#past_participle').value = '';
+
+		latest_verbs.push(verbs[ rand ]);
+		verbs.splice(rand, 1);
+
+	};
+
+	var finishedGame = function() {
+
+		verbs = latest_verbs;
+		latest_verbs = [];
+
+		document.querySelector('#finish_points_correct').innerHTML = points_correct;
+		document.querySelector('#finish_points_wrong').innerHTML = points_wrong;
+		document.querySelector('#finish_points_max_consective').innerHTML = points_max_consective;
+		document.querySelector('#finish_points_consective').innerHTML = points_consective;
+		$('#modal-finish').modal('open');
+		$('#modal-finish .btn').focus();
+	};
+
+	var init = function() {
+
+		randomNewVerb();
+
+	};
+
+	var verify = function() {
+
+		return {
+			past: document.querySelector('#past').value == in_past,
+			past_participle: document.querySelector('#past_participle').value == in_past_participle,
+		}
+	}
+
+	var form_submit = function(e) {
+		e.preventDefault();
+
+		var answer = verify();
+		showResult(answer);
+		calculatePoints(answer)
+		return false;
+	};
+
+	var getMessageText = function(answer) {
+
+		if( answer.past && answer.past_participle ) {
+			return '<span class="green-text text-darken-4">Good Job. All Right.</span>'
+		}
+
+		if( !answer.past && !answer.past_participle) {
+			return '<span class="red-text text-darken-4">Ops. All Wrong.</span>';
+		}
+
+		if( !answer.past) {
+			return '<span class="orange-text text-darken-4">You wrong the verb in past.</span>';
+		}
+
+		if( !answer.past_participle) {
+			return '<span class="orange-text text-darken-4">You wrong the verb in past participle.</span>';
+		}
+	}
+
+	var getAnswerText = function(text, is_correct) {
+		var myclass = 'red-text text-darken-4';
+		if( is_correct ) {
+			myclass = 'green-text text-darken-4';
+		}
+		return '<span class="' + myclass + '">' + text + '</span>';
+	}
+
+	var showResult = function (answer) {
+
+		document.querySelector('#modal-title').innerHTML = getMessageText(answer);
+
+		document.querySelector('#answer_infinitive').innerHTML = in_infinitive;
+		document.querySelector('#answer_past').innerHTML = getAnswerText(in_past, answer.past);
+		document.querySelector('#answer_past_participle').innerHTML = getAnswerText(in_past_participle, answer.past_participle);
+		document.querySelector('#answer_translated').innerHTML = getAnswerText(translated, true);
+		$('#modal-answers').modal('open');
+		$('#modal-answers .btn').focus();
+	};
+
+	var calculatePoints = function (answer) {
+
+		if( answer.past && answer.past_participle ) {
+			points_consective++;
+		}
+		else {
+			points_consective = 0;
+		}
+
+		if( answer.past ) {
+			points_correct++;
+		}
+		else {
+			points_wrong++;
+		}
+
+		if( answer.past_participle ) {
+			points_correct++;
+		}
+		else {
+			points_wrong++;
+		}
+
+		$('#points_correct').html(points_correct);
+		$('#points_wrong').html(points_wrong);
+		$('#points_consective').html(points_consective);
+
+		if( points_consective > points_max_consective ) {
+			points_max_consective = points_consective;
+		}
+	};
+
+	var modal_closed = function() {
+		$('#past').focus();
+		randomNewVerb();
+	};
+
+	return {
+        init: init,
+        form_submit: form_submit,
+        modal_closed: modal_closed
+    }
+})()
+document.addEventListener("DOMContentLoaded", function() {
+	irregular_verbs.init();
+	document.querySelector('#irregular_verbs_form').addEventListener("submit", irregular_verbs.form_submit);
+	document.querySelector('#irregular_verbs_bt').disabled = false;
+});
+
+$(function(){
+	$(".button-collapse").sideNav();
+	$('.modal').modal({
+		complete: function() {
+			irregular_verbs.modal_closed();
+		}
+	});
+	$('#past').focus();
+});
