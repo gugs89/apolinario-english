62456575439a9dbc733fd282c1c93355339dc25e
Gustavo Apolinario <gustavoapolinario@MacBook-Pro-de-Gustavo.local>
2017-11-11 10:00:16 -0200

    Moving service worker script

--- service-worker-register.js
+++ service-worker-register.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('js/service-worker.js', {
+    navigator.serviceWorker.register('service-worker.js', {
         scope: './'
     }).then(function() {
         console.info('service worker registered');
