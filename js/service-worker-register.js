815c530e4457eb1b5d3ffeb71b9958c19b7acfe8
Gustavo Apolinario <gustavoapolinario@MacBook-Pro-de-Gustavo.local>
2017-11-11 09:54:39 -0200

    test url service worker

--- js/service-worker-register.js
+++ js/service-worker-register.js
@@ -1,5 +1,7 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('js/service-worker.js').then(function() {
+    navigator.serviceWorker.register('js/service-worker.js', {
+        scope: './'
+    }).then(function() {
         console.info('service worker registered');
     }).catch(function(e) {
         console.error(e, 'service worker registration failed');
